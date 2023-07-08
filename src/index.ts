93c45f50c1465f5fbef4a0d14a1972a8bafadf06
anish4 <anishniraula85@gmail.com>
2023-07-08 14:00:50 +0545

    final

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,12 @@ import express, { Response } from "express";
 import { block, generateBlock, getBlockChain, addBlock } from "./block";
 import dotenv from "dotenv";
 import { SHA256 } from "crypto-js";
+import { hexToBinary } from "./utils";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 const HTTP_PORT = process.env.HTTP_PORT || 4000;
-console.log(
-  SHA256(
-    18 +
-      1643897254202 +
-      "07d2f587e93fff09b2fef26a50e28f265419ffb19d7e31ae5561130218f82a15" +
-      "Rohan paid Zen 500 bestc\n Noto paid Mane 90 bestc" +
-      3 +
-      14
-  ).toString()
-);
-
 app.get("/blocks", (_, res: Response) => {
   res.send(getBlockChain());
 });
